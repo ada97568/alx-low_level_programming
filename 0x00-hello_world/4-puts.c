5d91518493ba008305918222e5e25936398e3223
Adama BA <aba47979@gmail.com>
2022-09-09 13:53:24 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Print string in console
+ *
+ * Return: 0 to terminate program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
