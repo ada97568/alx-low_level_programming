79d639072ad7d842e27d7fab3b64a67ec286a9d2
Adama BA <aba47979@gmail.com>
2022-09-09 16:16:25 -0700

    Intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Print string in console   
  *
- *  * main - Print string in console
- *
- *   *
- *
- *    * Return: 0 to terminate program
- *
+ * Description: using the main function
+ * This program prints Program is like building a multilingual puzzle 
+ * Return: 0 to terminate program
  */
-
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
