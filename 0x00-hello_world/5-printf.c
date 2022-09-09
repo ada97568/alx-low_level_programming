e2ddb71db7ab17fe29ec93e6d49af3a8efe7bce3
Adama BA <aba47979@gmail.com>
2022-09-09 14:52:41 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - Print string in console
  *
- * Return: 0 to terminate program 
- */
+ *  * main - Print string in console
+ *
+ *   *
+ *
+ *    * Return: 0 to terminate program
+ *
+ *     **/
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
