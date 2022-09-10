bbf9dd055ebbcf6c16cc0fce3967da8a4c9e81c6
Adama BA <aba47979@gmail.com>
2022-09-09 19:38:56 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
+#include <stdio<h>
 
 /**
- * main - Print string in console   
- *
- * Description: using the main function
- * This program prints Program is like building a multilingual puzzle 
- * Return: 0 to terminate program
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
