8c533e134d6be68436dc59eeb3cce82e991dd4bb
Adama BA <aba47979@gmail.com>
2022-10-01 13:24:50 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,9 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
-
 void print_alphabet_x10(void)
 {
 	char ch;
-
 	int i;
 
 	i = 0;
@@ -16,13 +14,13 @@ void print_alphabet_x10(void)
 	while (i < 10)
 	{
 		ch = 'a';
-
 		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
 		}
+
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\n');
-	i++;
 }
