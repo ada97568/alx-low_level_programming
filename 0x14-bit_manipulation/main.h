6e1f894ae90f99e211fd23c9844cd8dc1460fd10
Adama BA <aba47979@gmail.com>
2022-10-28 17:01:24 -0700

    function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+# ifndef HOLBERTON_H
+# définir  HOLBERTON_H
+int  _putchar ( char c);
+unsigned  int  binary_to_uint ( const  char *b);
+void  print_binary ( entier long non signé  n );
+int  get_bit ( unsigned  long  int n, unsigned  int index);
+int  set_bit ( int long non signé  *n, index int non signé );
+int  clear_bit ( int long non signé  *n, index int non signé );
+unsigned  int  flip_bits ( unsigned  long  int n, unsigned  long  int m);
+int  get_endianness ( void );
+# endif
