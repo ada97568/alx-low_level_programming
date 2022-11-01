62ba635f7003b6bcbfb174d4b9fafc21a8e194de
Adama BA <aba47979@gmail.com>
2022-10-31 17:10:24 -0700

    task 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,21 @@
-# ifndef HOLBERTON_H
-# définir  HOLBERTON_H
-int  _putchar ( char c);
-unsigned  int  binary_to_uint ( const  char *b);
-void  print_binary ( entier long non signé  n );
-int  get_bit ( unsigned  long  int n, unsigned  int index);
-int  set_bit ( int long non signé  *n, index int non signé );
-int  clear_bit ( int long non signé  *n, index int non signé );
-unsigned  int  flip_bits ( unsigned  long  int n, unsigned  long  int m);
-int  get_endianness ( void );
-# endif
+#ifndef HOLBERTON_H
+
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+unsigned int binary_to_uint(const char *b);
+
+void print_binary(unsigned long int n);
+
+int get_bit(unsigned long int n, unsigned int index);
+
+int set_bit(unsigned long int *n, unsigned int index);
+
+int clear_bit(unsigned long int *n, unsigned int index);
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+int get_endianness(void);
+
+#endif
